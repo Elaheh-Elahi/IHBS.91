0c92095037d295af10872932797239bbe1adb4c9
elahe-elahi <elaheh.2110@gmail.com>
2024-02-15 10:55:55 -0800

    First edit

--- R/Data_Documantation.R
+++ R/Data_Documantation.R
@@ -1,21 +1,40 @@
+#' Title for R91
+#'
+#' @format A data frame with 2 variables:
+#' \describe{
+#'   \item{Address}{Address}
+#'   \item{MahMorajeh}{The month of visiting the household}
+#' }
+"R91"
+
 #' Title for R91Data
 #'
 #' @format A data frame with 22 variables:
 #' \describe{
-#'   \item{Address}{Description of var1}
-#'   \item{MahMorajeh}{Description of var2}
-#'   \item{ShoghlSarparast}{Description of var3}
-#'   \item{NoeKhn}{Description of var4}
-#'   \item{Takmil}{Description of var5}
-#'   \item{TakmilDescA}{Description of var6}
-#'   \item{TakmilDescB}{Description of var7}
-#'   \item{TakmilDescC}{Description of var8}
-#'   \item{Jaygozin}{Description of var9}
-#'   \item{JaygozinDescA}{Description of var10}
-#'   \item{JaygozinDescB}{Description of var11}
-#'   \item{JaygozinDescC}{Description of var12}
-#'   \item{BlkAbdJaygozin}{Description of var13}
-#'   \item{RadifJaygozin}{Description of var14}
+#'   \item{Address}{Address}
+#'   \item{MahMorajeh}{The month of visiting the household}
+#'   \item{ShoghlSarparast}{The job of the head of the household}
+#'   \item{NoeKhn}{Household type (ordinary resident code 1, group code 2)}
+#'   \item{Takmil}{Questionnaire for substitute household(Yes cod 1, No code 2)}؟؟؟؟؟؟؟؟؟
+#'   \item{TakmilDescA}{Table-2 information on the reason for removing the household(
+#'        1.The family does not want to cooperate.
+#'        2.The family due to old age, mental disability, illiteracy of all family members or... is not able to answer.
+#         3.Access to the place for reasons such as weather conditions, the route being difficult to pass or
+#         Security issues are not possible until the end of the season.)}
+#'   \item{TakmilDescB}{Table-3 information on the reason for replacing the household(
+#'        1.The household was absent during the census season.
+#'        2.The place is empty.
+#'        3.The place is not an ordinary residence for the household.
+#'        4.The place is destroyed, under construction or demolition.
+#'        5.The residence of the family is an institutional or non-resident.
+#'        6.The intend address was not found)}
+#'   \item{TakmilDescC}{(Code 7, table-3, other (with the reason for replacing the household)}
+#'   \item{Jaygozin}{}   ????????????????
+#'   \item{JaygozinDescA}{Table-4 the reason for the removal of the replacement household for reasons of removal)}
+#'   \item{JaygozinDescB}{Table-5 the re  ason for the removal of the replacement household for alternative reasons}
+#'   \item{JaygozinDescC}{(Code 7, table-5, other (with the reason for the removal of the replacement household for alternative reasons)}
+#'   \item{BlkAbdJaygozin}{Block or settlement number of the alternative household (which must be the same as the original sample household)}
+#'   \item{RadifJaygozin}{Substitute household row number in the primary sampling unit}
 #'   \item{fStat}{Description of var15}
 #'   \item{fEnt}{Description of var16}
 #'   \item{fvfy}{Description of var17}
@@ -26,6 +45,62 @@
 #'   \item{Tekrari}{Description of var22}
 #' }
 "R91Data"
+
+#' Title for R91KarBargh1
+#'
+#' @format A data frame with 4 variables:
+#' \describe{
+#'   \item{Address}{Address}
+#'   \item{DYCOL01}{Person row number}
+#'   \item{DYCOL02}{Activity row number}
+#'   \item{DYCOL03}{Income}
+#' }
+"R91KarBargh1"
+
+#' Title for R91KarBargh2
+#'
+#' @format A data frame with 10 variables:
+#' \describe{Income (employees in the agricultural sector)
+#'   \item{Address}{Address}
+#'   \item{DYCOL01}{Person row number}
+#'   \item{DYCOL02}{Job code}
+#'   \item{DYCOL03}{Type and feature of activities (agriculture)}
+#'   \item{DYCOL04}{Acreage}
+#'   \item{DYCOL05}{Gardening}
+#'   \item{DYCOL06}{Garden size}
+#'   \item{DYCOL07}{Animal husbandry}
+#'   \item{DYCOL08}{The number of small livestock}
+#'   \item{DYCOL09}{The number of big livestock}
+#'   \item{DYCOL10}{Poultry farming}
+#'   \item{DYCOL11}{Number of hens, chickens and roosters}
+#'   \item{DYCOL12}{Breeding fish}
+#'   \item{DYCOL13}{Pool size for breeding}
+#'   \item{DYCOL14}{Other agricultural activities}
+#'   \item{DYCOL15}{Description of var6 "تعمیرات خرمنکوب"}
+#'   \item{DYCOL16}{("تعمیرات تراکتور خریداری شده")}
+#'   \item{DYCOL17}{Description of var8}
+#'   \item{DYCOL18}{Description of var9}
+#'   \item{DYCOL19}{Description of var10}
+#'   \item{DYCOL20}{Description of var2}
+#'   \item{DYCOL21}{Description of var3}
+#'   \item{DYCOL22}{Description of var4}
+#'   \item{DYCOL23}{Description of var5}
+#'   \item{DYCOL24}{Description of var6}
+#'   \item{DYCOL25}{Description of var7}
+#'   \item{DYCOL26}{Description of var8}
+#'   \item{DYCOL27}{Description of var9}
+#'   \item{DYCOL28}{Description of var10}
+#'   \item{DYCOL29}{Description of var2}
+#'   \item{DYCOL30}{Description of var3}
+#'   \item{DYCOL31}{Description of var4}
+#'   \item{DYCOL32}{Description of var5}
+#'   \item{DYCOL33}{Description of var6}
+#'   \item{DYCOL07}{Description of var7}
+#'   \item{DYCOL08}{Description of var8}
+#'   \item{DYCOL09}{Description of var9}
+#'   \item{DYCOL10}{Description of var10}
+#' }
+"R91KarBargh2"
 
 #' Title for R91P1
 #'
